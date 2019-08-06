60e5f9a5ea030a083b5e195d5d7a45cb38f2c4e2
Lucas Lopes Trindade <lucaslotrindadee@gmail.com>
2019-08-06 16:33:09 -0300

    código inicial, segundo commit

--- credit/CreditCardValid.py
+++ credit/CreditCardValid.py
@@ -1,1 +1,8 @@
-print('hello')
+CheckDigit = "371449635398431"
+
+class CreditCardValid:
+	def __init__(self,CheckDigit):
+		self.cred_digit = CheckDigit
+
+	def basic_validation(self,cred_digit):
+		self.cred_digit.
