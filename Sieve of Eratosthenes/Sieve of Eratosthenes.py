dd5b1312c634a40d168d18d8e51469d72419c5ac
Lucas <lucaslotrindadee@gmail.com>
2019-09-09 14:57:42 -0300

    comentários das alterações

---
+++ Sieve of Eratosthenes/Sieve of Eratosthenes.py
@@ -0,0 +1,33 @@
+#USING THE SIEVE OF ERASTOTHENES ALGORITHM:
+# Given a number n, print all primes smaller than or equal to n. It is also given that n is a small number. 
+#The sieve of Eratosthenes is one of the most efficient ways to find all primes smaller than n when n is smaller than 10 million.
+import math
+
+def main():
+    
+    p = 2
+    i = 1
+    n = int(input('Insert the n value:\n'))    
+    
+    list_n = [number for number in range(2,n+1)]
+    limit = int(math.sqrt(n))
+
+    while p<=limit:
+
+        list_mark = [mark for mark in range(p,n+1,p) if mark != p]
+        list_n = list(set(list_n) - set(list_mark))
+
+        p=list_n[i]
+        i+=1
+    return list_n
+
+
+if  __name__ == '__main__':
+
+    primes =  main()
+    print(primes)
+    
+
+
+
+
--- credit/CreditCardValid.py
+++ credit/CreditCardValid.py
@@ -1,8 +1,5 @@
 CheckDigit = "371449635398431"
 
-class CreditCardValid:
-	def __init__(self,CheckDigit):
-		self.cred_digit = CheckDigit
-
-	def basic_validation(self,cred_digit):
-		self.cred_digit.
+print(int(CheckDigit))
+cc='11'
+cc.isnumeric
