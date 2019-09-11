704bec08142464ed14713e39c058dbd90454b706
Lucas <lucaslotrindadee@gmail.com>
2019-09-11 15:39:26 -0300

    sorting finish, add animated graph

--- Sorting/Sorting.py
+++ Sorting/Sorting.py
@@ -1,27 +1,39 @@
 import random
+import matplotlib.pyplot as plt 
+import numpy as np
 
 
-def buble_sort(rand_list):
-	v=0
-	for i,value in enumerate(rand_list):
-		print(i,'-',value)
-		#print(value)
-		if i == len(rand_list)-1:
-			break
-			print('oi')
-	
-		if value> rand_list[i+1]:
-			rand_list[i] = rand_list[i+1]
-			rand_list[i+1] = value
-			v+=1
-		
+def bubble_sort(rand_list):
+    not_done = True
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,1,1)
+    
+    while not_done:
+        not_done = False
+        index = np.arange(len(rand_list))
+        v=0
+        
+        for i,value in enumerate(rand_list):
+            if i == len(rand_list)-1:
+                break                
 
-		
-	return rand_list
+            if value> rand_list[i+1]:
+                not_done = True
+                rand_list[i] = rand_list[i+1]
+                rand_list[i+1] = value
+                v+=1
+        ax1.clear()
+        ax1.bar(index, rand_list)
+        plt.pause(0.01)
+        plt.draw()
+#        plt.show()
+
+    return rand_list
 
 if __name__ == '__main__':
-	n = 100
-	rand_list = [random.randint(1,100) for i in range(100)] 
-	print(rand_list)
-	sorted_list = buble_sort(rand_list)
-	print(sorted_list)
+    n = 100
+    #rand_list = [random.randint(1,100) for i in range(100)]
+    rand_list = [i for i in range(100)]
+    rand_list = random.sample(rand_list,n)
+    sorted_list = bubble_sort(rand_list)
+    print(sorted_list)
